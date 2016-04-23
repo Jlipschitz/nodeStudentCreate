bd0ee9ba525f348bbd758a39ab38aae55cb01404
Jamie Lipschitz <Jay.lipschitz@gmail.com>
2016-04-23 10:49:12 -0400

    Update bus.js

--- bus.js
+++ bus.js
@@ -11,16 +11,19 @@ var Bus = function (driverName, color, gas) {
             GPA, deten, sleep, catchPhrase));
         console.log(this.studentsOnTheBus)
     }
+    //console logs all of the students catchphrases
     this.busChatter = function () {
         for (var i = 0; i < this.studentsOnTheBus.length; i++) {
             console.log(this.studentsOnTheBus[i].catchPhrase);
         }
     }
+    //call our busChatter functon when the bus is > 19
     this.attendance = function () {
         if (this.studentsOnTheBus.length > 19) {
             this.busChatter();
         }
     }
+    //sort through our students array and remove where it matches the name passed into our removeStudent function
     this.removeStudent = function (studentName) {
         for (var i = 0; i < this.studentsOnTheBus.length; i++) {
             if (this.studentsOnTheBus[i].name === studentName) {
